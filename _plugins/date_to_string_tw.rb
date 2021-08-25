fb02f7fc034b7ae5e1b6da8f27cb91fe779b559f
hollen9 <hollen.tw@gmail.com>
2021-08-25 11:39:06 +0800

    縮排

--- _plugins/date_to_string_tw.rb
+++ _plugins/date_to_string_tw.rb
@@ -4,15 +4,15 @@ require 'date'
 require 'time'
 
 module DateToStringTw
-    def date_to_string_tw(datetime)
-      if datetime.is_a?(String)
-        return DateTime.parse(datetime).strftime("%Y年%m月%d日 %H:%M:%S") + " UTC+8"
-      end
-      if datetime.is_a?(Time)
-        return datetime.strftime("%Y年%m月%d日 %H:%M:%S") + " UTC+8"
-      end
-      return "--"
+  def date_to_string_tw(datetime)
+    if datetime.is_a?(String)
+      return DateTime.parse(datetime).strftime("%Y年%m月%d日 %H:%M:%S") + " UTC+8"
     end
+    if datetime.is_a?(Time)
+      return datetime.strftime("%Y年%m月%d日 %H:%M:%S") + " UTC+8"
+    end
+    return "--"
   end
+end
   
 Liquid::Template.register_filter(DateToStringTw)
